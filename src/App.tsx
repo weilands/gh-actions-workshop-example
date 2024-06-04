00e5eb9a746aba2a73f4bf51faa0740992af6999
Lukas Hauser <lukas.hauser@tngtech.com>
2024-06-04 20:56:51 +0200

    update tasks
    Signed-off-by: Lukas Hauser <lukas.hauser@tngtech.com>

--- src/App.tsx
+++ src/App.tsx
@@ -28,111 +28,127 @@ function App() {
           Create your first GitHub Actions Workflow:
           <ul>
             <li>
-              Create a file with the following content (source:
-              https://docs.github.com/en/actions/quickstart) in `.github/workflows/hello.yaml`
+              In your fork, create the file <i>.github/workflows/hello.yaml</i> with this content (<a target="_blank" href="https://docs.github.com/en/actions/quickstart">Source</a>):
+              <CodeBlock language="yaml" value={excercise01CodeString} />
             </li>
             <li>
-              Explore the GitHub Actions Tab. Especially, how to restart a run, how to retrieve
-              logs, ...
+              Explore the GitHub Actions Tab in `./actions`.
+              <ul>
+                <li>Trigger a workflow</li>
+                <li>Read the logs</li>
+              </ul>
             </li>
             <li>Disable the workflow in the end</li>
           </ul>
-          <CodeBlock language="yaml" value={excercise01CodeString} />
         </Concordion>
 
         <Concordion title="Excercise 2: Basic CI Pipeline">
           Create a workflow which:
           <ul>
-            <li>is called ci-workflow</li>
-            <li>is triggered whenever a push is performed</li>
-            <li>
-              enable the workflow to be triggered manually{' '}
-              <a
-                href="https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows"
-                target="_blank"
-              >
-                Workflow Triggers
-              </a>
+            <li>Is named ci-workflow</li>
+            <li>Is triggered whenever
+              <ul>
+                <li>A push is performed</li>
+                <li>It is manually triggered</li>
+                <li><a
+                    href="https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows"
+                    target="_blank"
+                >
+                  Docs: Workflow Triggers
+                </a></li>
+              </ul>
             </li>
-            <li>contains a job called ci-pipeline</li>
+            <li>Contains a job called ci-pipeline</li>
             <li>
-              runs on a GitHub hosted ubuntu runner{' '}
-              <a
-                href="https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners"
-                target="_blank"
-              >
-                GitHub Runners
-              </a>
+              Runs on a GitHub hosted ubuntu runner
+              <ul>
+                <li>
+                  <a
+                    href="https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners"
+                    target="_blank"
+                  >
+                    Docs: GitHub Runners
+                  </a>
+                </li>
+              </ul>
             </li>
-            <li>
-              checks out the code{' '}
-              <a href="https://github.com/actions" target="_blank">
-                GitHub provided Actions
-              </a>
-            </li>
-            <li>
-              sets up node 20{' '}
-              <a href="https://github.com/actions" target="_blank">
-                GitHub provided Actions
-              </a>
-            </li>
-            <li>
-              performs the steps `npm ci`, `npm run prettier`, `npm run typecheck`, `npm run test`,
-              `npm run build`
+            <li>Runs the following steps:
+              <ul>
+                <li>Check out the source code</li>
+                <li>Set up NodeJS 20</li>
+                <li>
+                  Runs the following npm scripts:
+                  <CodeBlock language="bash" value="npm ci
+npm run prettier
+npm run typecheck
+npm run test
+npm run build" />
+                </li>
+                <li>
+                  <a href="https://github.com/actions" target="_blank">
+                    Actions provided by GitHub
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
           <Concordion title="Solution Excercise 2">
             <CodeBlock language="yaml" value={excercise02CodeString} />
             <ul>
               <li>
-                Discuss different GitHub Runner possibilities. E.g. Windows, Linux, MacOS or
-                selfhosted
+                Docs: <a target="_blank" href="https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners#standard-github-hosted-runners-for-public-repositories">Standard GitHub-hosted runners for Public repositories</a>
               </li>
             </ul>
           </Concordion>
         </Concordion>
 
         <Concordion title="Excercise 3: CI/CD Pipeline">
-          Extend your CI-Job from the previous excercise with a CD-Job
+          Extend your CI-Job from the previous exercise with a CD-Job.
+          Let's deploy our application to GitHub Pages.
+
           <ul>
             <li>
-              Activate GitHub Pages in your repository: Settings -{'>'} Pages -{'>'} Source: GitHub
-              Actions
+              Activate GitHub Pages in your repository: <i>Settings &#x3e; Pages &#x3e; Source: GitHub Actions</i>
             </li>
-            <li>Rename the workflow created in Excercise 2 to ci-cd</li>
+            <li>Rename the workflow created in Exercise 2 to <i>ci-cd</i></li>
             <li>
-              Upload an artifact of the build using the GitHub provided action
-              `actions/upload-pages-artifact@v3` with the parameter `path: 'dist/'`
+              Upload the built artifact using the <a target="_blank" href="https://github.com/actions/upload-pages-artifact">
+                <i>actions/upload-pages-artifact@v3</i></a> with the parameter <i>path: 'dist/'</i>
             </li>
-            <li>
-              Create a second job in the same Workflow called cd-pipeline, which requires the
-              ci-pipeline to successfully finish
+            <li>Create a second job to deploy this artifact
+              <ul>
+                <li>Called it <i>cd-pipeline</i></li>
+                <li>Create a dependency to the <i>ci-pipeline</i> job</li>
+                <li>Provide the github.token with the additional permissions: <i>pages: write</i></li>
+                <li><a href="https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idenvironment" target="_blank">Add an environment</a> with the name <i>github-pages</i></li>
+                <li>
+                  Deploy the GitHub Pages using the GitHub provided action <a
+                    target="_blank" href="https://github.com/actions/deploy-pages"><i>actions/deploy-pages@v4</i></a>
+                </li>
+              </ul>
             </li>
-            <li>Provide the github.token with the additional permissions: `pages: write`</li>
-            <li>Add an environment with the name `github-pages`</li>
-            <li>
-              Deploy the GitHub Pages using the GitHub provided action `actions/deploy-pages@v4`
-            </li>
+
           </ul>
           <Concordion title="Solution Excercise 3">
             <CodeBlock language="yaml" value={excercise03CodeString} />
             <ul>
               <li>
-                After the successful CI/CD execution, go to your Repository: Settings -{'>'}
-                Environments and inspect the github-pages environment Settings
+                After the successful CI/CD execution, go to your Repository: <i>Settings &#x3e;
+                Environments</i> and inspect the <i>github-pages environment Settings</i>.
               </li>
             </ul>
           </Concordion>
         </Concordion>
 
         <Concordion title="Excercise 4: Matrix Jobs">
-          Test your deployed App with both Firefox and Chromium
+          After the successful deployment of your App to GitHub Pages, let's add a matrix job to test your App with different browsers.
+
           <ul>
             <li>
-              Add the following code snippet to your ci-cd-workflow as a new job
+              Add the following code snippet to your <i>ci-cd-workflow</i> as a new job and omplete the <i>TODO</i> statements as listed in the new code snippet
               <CodeBlock language="yaml" value={excercise04CodeString_a} />
             </li>
-            <li>Complete the TODO statements as listed in the new code snippet</li>
+            <li>Docs: <a href="https://docs.github.com/en/actions/using-jobs/using-a-matrix-for-your-jobs" target="_blank">Using a matrix for your jobs</a></li>
           </ul>
           <Concordion title="Solution Excercise 4">
             <CodeBlock language="yaml" value={excercise04CodeString_b} />
@@ -140,13 +156,14 @@ function App() {
         </Concordion>
 
         <Concordion title="Excercise 5: Composite Action">
-          Remove duplicated code with a Composite Action:
+          Remove duplicated code with a <a href="https://docs.github.com/en/actions/creating-actions/creating-a-composite-action" target="_blank">Composite Action</a>:
           <ul>
             <li>
               In both the ci-job as well as the matrix smoke test job we install node and the npm
               dependencies. These two steps are duplicated. Create a Composite Action to remove this
               duplication.
             </li>
+            <li>Put the action inside this repo in <i>.github/actions/install/action.yaml</i></li>
           </ul>
           <Concordion title="Solution Excercise 5">
             <CodeBlock language="yaml" value={excercise05CodeString} />
@@ -157,8 +174,8 @@ function App() {
           <Concordion title="Real World Example">
             <div>
               <ul>
-                <li><a href="https://github.com/magma/magma/blob/master/.github/workflows/bazel.yml">Magma</a></li>
-                <li><a href="https://github.com/goharbor/go-client">Harbor Go Client</a></li>
+                <li><a target="_blank" href="https://github.com/magma/magma/blob/master/.github/workflows/bazel.yml">Magma</a></li>
+                <li><a target="_blank" href="https://github.com/goharbor/go-client">Harbor Go Client</a></li>
                 <li>...</li>
               </ul>
             </div>
@@ -166,16 +183,16 @@ function App() {
           <Concordion title="Relevant Topics">
             <div>
               <ul>
-                <li><a href="https://docs.github.com/en/actions/using-workflows/reusing-workflows">Reusing workflows</a></li>
-                <li><a href="https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions">Using secrets in GitHub Actions</a></li>
-                <li><a href="https://github.com/TNG-GitHub-Actions-Workshop/empty-sample/settings/secrets/actions">Secrets in Repos</a></li>
-                <li><a href="https://github.com/organizations/TNG-GitHub-Actions-Workshop/settings/secrets/actions">Secrets in Orgs</a></li>
-                <li><a href="https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#creating-an-environment">Creating an environment</a></li>
-                <li><a href="https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/about-self-hosted-runners">About self-hosted runners</a></li>
-                <li><a href="https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/autoscaling-with-self-hosted-runners">Autoscaling with self-hosted runners</a></li>
-                <li><a href="https://docs.github.com/en/actions/using-workflows/caching-dependencies-to-speed-up-workflows">Caching dependencies to speed up workflows</a></li>
-                <li><a href="https://docs.github.com/en/packages/quickstart">GitHub Packages</a></li>
-                <li><a href="https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry">Working with the Container registry</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/actions/using-workflows/reusing-workflows">Reusing workflows</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions">Using secrets in GitHub Actions</a></li>
+                <li><a target="_blank" href="https://github.com/TNG-GitHub-Actions-Workshop/empty-sample/settings/secrets/actions">Secrets in Repos</a></li>
+                <li><a target="_blank" href="https://github.com/organizations/TNG-GitHub-Actions-Workshop/settings/secrets/actions">Secrets in Orgs</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#creating-an-environment">Creating an environment</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/about-self-hosted-runners">About self-hosted runners</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/autoscaling-with-self-hosted-runners">Autoscaling with self-hosted runners</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/actions/using-workflows/caching-dependencies-to-speed-up-workflows">Caching dependencies to speed up workflows</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/packages/quickstart">GitHub Packages</a></li>
+                <li>Docs: <a target="_blank" href="https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry">Working with the Container registry</a></li>
               </ul>
             </div>
           </Concordion>
